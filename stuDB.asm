4e81bc9d45510d8bf64d6dbc4d7fd522d7116e23
narenjak <narenjak376@gmail.com>
2021-12-07 21:40:50 +0330

    some code cutted and reformt stu:name,family,stuNo,p1,p2,p3,1;  1 means stu is valied

--- stuDB.asm
+++ stuDB.asm
@@ -18,7 +18,7 @@ choice 	     db 10,13,"Enter your choice: $"
 
 pORa         db 10,13,"1" ;1 peresent and 0 absent in DB
 	
-name1        db 10,13,"Enter Name: $"
+name1         db 10,13,"Enter Name: $"
 	
 Family       db 10,13,"Enter Family: $"     
 
@@ -36,11 +36,11 @@ message2     db 10,13,"do you want see all data? 'y' for yes or 'n' for no: $"
 
 message3     db 10,13,"Enter your keyNAME: $"	
 	                                 
-data         db  20,?,20 dup ('0'),'$'    
+data         db  20,?,20 dup ('0'),'$'  ;max,len,initialization,/0  
         
 index        dw  0          
 
-stu_db       db  2048 dup('0')
+stu_db       db  1024 dup('0')
 
 namekey      db '' 
                                           
@@ -95,39 +95,44 @@ addstu:
         call adst
         jmp pl
 removestu:  
-        call rst
+        ;call rst
         jmp pl
 defragdb:	
-        call dfd
+        ;call dfd
         jmp pl
 search:  
-        call ser
+        ;call ser
         jmp pl
 loadfile:	
-        call lfi
+        ;call lfi
         jmp pl
 savefile:  
-        call sfi
+        ;call sfi
         jmp pl
 exit:   
         mov ax,4c00h
         int 21h
 main    endp
+
+
 ;add student proce
-adst   proc
+adst    proc 
+    
 adst1:	lea  dx,name1 ;message for get name and ...
 		call getdata
 		lea  dx,family
 		call getdata
 		lea  dx,Stu_no
 		call getdata
+		;get points
 		lea dx,point1
 		call getdata
 		lea dx,point2
 		call getdata
 		lea dx,point3
 		call getdata
-		mov stu_db[bx-1],'1' ;name,lastname,stuno,p1,p2,p3,1;
+		mov stu_db[bx],'1' ;name,lastname,stuno,p1,p2,p3,1;
+		inc bx
 		mov stu_db[bx],';' 
 		inc bx
 		mov index,bx
@@ -145,63 +150,11 @@ adst2:	lea dx,message1  ;continue add student
 		jne adst2
 		
         ret 
-adst   endp
-;remove student
-rst   proc
         
-        ret 
-rst   endp
-;------------------------
-dfd   proc
-        
-        ret 
-dfd   endp
-;search
-ser   proc
-      ;request for all data or a key?
-      lea dx,message2
-      mov ah,9
-      int 21h
-	  ;get choise
-	  mov ah,1
-	  int 21h
-	  ;compare 
-	  cmp al,'y'
-	  jmp ser1
-	  cmp al,'n'
-	  jmp ser2
+adst   endp       
 
-sr1:  call ser1
-sr2:  call ser2
-	  ret
-ser endp
+;some code cutted
 
-ser1 proc
-     lea dx,message1
-     mov ah,9
-     int 21h 
-     ret
-end proc
-ser2 proc
-     ;get nameKAy   
-	  lea dx,message3
-      mov ah,9
-	  int 21h
-	  mov ah , 01h
-	  lea dx,  namekey 
-	  int 21h
-end proc        
-;------------------------
-lfi   proc
-        
-        ret 
-lfi   endp
-;------------------------
-sfi   proc
-        
-        ret
-sfi   endp
-;------------------------
 getdata  proc
         ;------ message for data--------------
 		mov ah,9
@@ -214,17 +167,19 @@ getdata  proc
 		mov bx,index   ;bx index of start stu_db
 		lea si,data+2
 		lea di,stu_db[bx]
+		;len+index:
 		mov cl,data+1
 		mov ch,0
 		add bx,cx
+		;add data   
 		cld         
 		rep movsb
-		mov stu_db[bx],','   ;st1,st2,...
+		mov stu_db[bx],','   ;firstname,lastname,...
 		inc bx
 		mov index,bx
-        ;---------------------
-         ret
+        ret
 getdata  endp
+
 codesg  ends
         end        main
        
